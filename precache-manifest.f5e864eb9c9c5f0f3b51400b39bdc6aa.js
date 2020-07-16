b444a175da9bb7a83f2cc3571ff63a7b8b520bf6
pinger <402434104@qq.com>
2020-07-16 12:15:30 +0800

    deploy

---
+++ precache-manifest.f5e864eb9c9c5f0f3b51400b39bdc6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814d9e5cf1f7e2e8f5e8",
+    "url": "/mt/css/app.290c05b8.css"
+  },
+  {
+    "revision": "08a0f895e95cb7fe4328b258b7982a0c",
+    "url": "/mt/index.html"
+  },
+  {
+    "revision": "814d9e5cf1f7e2e8f5e8",
+    "url": "/mt/js/app.31fdcb46.js"
+  },
+  {
+    "revision": "aa72e1b6c97798d90e6b",
+    "url": "/mt/js/chunk-vendors.4750179a.js"
+  },
+  {
+    "revision": "cae8eceab08a4e36b84b32e6221449aa",
+    "url": "/mt/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mt/robots.txt"
+  }
+]);
